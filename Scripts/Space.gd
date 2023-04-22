7e1c89d9192bc69af57daccc58592eeb8f884d73
CaptainIndia <ayirakas28@gmail.com>
2023-04-22 12:07:58 -0400

    [BROKEN] adds enemy spawning and fixes some issues
    -Error animations
    -Added random spawning

---
+++ Scripts/Space.gd
@@ -0,0 +1,39 @@
+extends Node
+
+var score = 0
+
+#How many ticks it should take to spawn next enemy (at 60 tps)
+var spawn_speed : int = 10
+var spawn_cooldown = 1
+
+var enemy = preload("res://Scenes/Enemy.tscn")
+@export var max_enemy : int = 20
+var current_enemy_count : int = 0
+var spawn_location
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	spawn_location = get_node("SpawnPath/MovingSpawnLocation")	
+
+
+# Called every physics frame.
+func _physics_process(delta):
+	if (current_enemy_count < max_enemy && spawn_cooldown >= spawn_speed):
+		spawn_enemy()
+		spawn_cooldown = 1
+	else:
+		spawn_cooldown += 1
+	
+#Spawn enemy
+func spawn_enemy():
+	var enemy_instance = enemy.instantiate()
+	spawn_location.progress_ratio = randf()
+	enemy_instance.position = spawn_location.position
+	add_child(enemy_instance)
+	current_enemy_count += 1
+
+#Method that should be called when enemy is killed
+func enemy_killed(score_increase : int):
+	current_enemy_count -= 1
+	score += score_increase
+	#TODO increase difficulty here
--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,41 +1,57 @@
 extends StaticBody2D
 
-@export var movement_speed = 1
-@export_enum("meteoroid","alien") var enemy_type: String
+@export var movement_speed = 10
 
-@export var shooting_speed = 60
-var shooting_counter = 0;
-var bullet = preload("res://Scenes/bullet.tscn")
+enum enemy_types {meteoroid,alien} 
+var enemy_type: String
 
-var player : StaticBody2D
+@export var shooting_speed : int = 60
+var shooting_cooldown = 0;
+var bullet:PackedScene = preload("res://Scenes/bullet.tscn")
+
+var player : CharacterBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#Set the ramdom enemy type
+	enemy_type = enemy_types.keys()[randi() % enemy_types.size()]
 	#Get the player instance
-	player = get_tree().get_root().get_node("res://Scenes/player.tscn")
+	player = get_node("../PlayerPivot/Player")
 	#Play the animation based on enemy type
-	$EnemyAnimation.play(enemy_type)
+	$EnemyAnimation.play(str(enemy_type))
+	#If enemy type is meteoriod give it linear
+	if(enemy_type == "meteoroid"):
+		look_at(player.global_position)
+		#move_and_collide(movement_speed)
 
 # Called every physics frame. 60 frames per second by default.
 func _physics_process(delta):
+	match enemy_type:
+		enemy_types.alien:
+			sentient_enemy(delta)
+		
+
+#Movement code for enemy targeting player
+func sentient_enemy(delta):
 	#Shoot at player if not cooldown
-	if (shooting_counter >= shooting_speed):
+	if (shooting_cooldown >= shooting_speed):
 		fire()
-		shooting_counter = 0
+		shooting_cooldown = 0
 	else:
-		shooting_counter+=1
+		shooting_cooldown+=1
 	
 	#Look at player
 	look_at(player.global_position)
 	#Move towards player
 	var velocity = global_position.direction_to(player.global_position)
 	move_and_collide(velocity * movement_speed * delta)
-	
+
+#Create and give bullet movement
 func fire():
 	var bullet_instance = bullet.instantiate()
 	bullet_instance.rotation = rotation
 	bullet_instance.position = position
-	bullet.set_linear_velocity(Vector2(movement_speed*0.1,0.0).rotated(rotation-1.570796))
-	get_parent().add_child(bullet)
+	bullet_instance.set_linear_velocity(Vector2(movement_speed*2,0.0).rotated(rotation))
+	get_parent().add_child(bullet_instance)
 	
 	
