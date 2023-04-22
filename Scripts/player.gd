5116eedd6442ae6fc542bac5eb1df8965dd3710e
Ninjajkl <ninjajkl@gmail.com>
2023-04-21 22:31:05 -0400

    Added the player movement and bullets

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,41 +1,8 @@
-extends StaticBody2D
+extends CharacterBody2D
 
-# How fast the player moves in meters per second.
-@export var speed = 14
-# The downward acceleration when in the air, in meters per second squared.
-@export var fall_acceleration = 75
-
-var target_velocity = Vector3.ZERO
-
-
-func _physics_process(delta):
-	var direction = Vector3.ZERO
-
-	if Input.is_action_pressed("move_right"):
-		direction.x += 1
-	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("move_back"):
-		direction.z += 1
-	if Input.is_action_pressed("move_forward"):
-		direction.z -= 1
-
-	if direction != Vector3.ZERO:
-		direction = direction.normalized()
-		$Pivot.look_at(position + direction, Vector3.UP)
-
-	# Ground Velocity
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
-
-	# Vertical Velocity
-	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
-		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
-
-	# Moving the Character
-	velocity = target_velocity
-	move_and_slide()
-
+@export var rotation_speed = 3
+@export var bullet_speed = 3
+@export var bullet_scene: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -44,4 +11,18 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if(Input.is_action_pressed("ui_right")):
+		rotation += rotation_speed * delta
+	elif(Input.is_action_pressed("ui_left")):
+		rotation -= rotation_speed*1.5 * delta
+	if(Input.is_action_just_pressed("ui_space")):
+		shoot()
+
+func shoot():
+	var bullet = bullet_scene.instantiate()
+	
+	bullet.rotation = rotation
+	bullet.position = position
+	bullet.set_linear_velocity(Vector2(bullet_speed,0.0).rotated(get_parent().rotation+rotation-1.570796))
+	
+	get_parent().add_child(bullet)
