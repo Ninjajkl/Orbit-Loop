cf162407da0348128364c58cf34ad89f0e9dd26f
Ninjajkl <ninjajkl@gmail.com>
2023-04-22 12:36:34 -0400

    Added base for credits and title

---
+++ Scripts/credits.gd
@@ -0,0 +1,21 @@
+extends AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_title_button_pressed():
+	get_tree().change_scene("res://Scenes/title.tscn")
+	pass # Replace with function body.
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
